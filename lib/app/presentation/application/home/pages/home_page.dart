63a6e774c65d51658550c06b6e296e4a9f29f458
vintla <alvinferdianakbar@gmail.com>
2024-09-27 22:04:08 +0700

    slicing home ui

--- lib/app/presentation/application/home/pages/home_page.dart
+++ lib/app/presentation/application/home/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_app/app/common/widgets/basic_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/config/config.dart';
 
@@ -8,8 +10,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: BasicAppBar(
+        hideBack: true,
+        action: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(
+              height: 50.h,
+              width: 50.w,
+              child: CircleAvatar(
+                child: Image.asset("assets/images/profile.png"),
+              ),
+            ),
+            const DropdownMenu(
+              dropdownMenuEntries: [
+                DropdownMenuEntry(value: "Men", label: "Men"),
+                DropdownMenuEntry(value: "Men", label: "Men"),
+              ],
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Container(
+                height: 50.h,
+                width: 50.w,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColors.primary,
+                ),
+                child: const Icon(
+                  Icons.shopping_bag,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       body: Container(
-        color: AppColors.primary,
+        color: AppColors.background,
       ),
     );
   }
